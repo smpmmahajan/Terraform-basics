21b0cab75a761177cbe6a77308c4aee455301c00
smahajan <mahajan.sagr46@gmail.com>
2024-08-31 18:36:29 +0530

    Variable

--- variable-sagar/var.tf
+++ variable-sagar/var.tf
@@ -2,3 +2,11 @@ variable "image_id" {
   type = string
    description = "The id of the machine image (AMI) to use for the server."
 }
+
+variable "a" {
+  default = 10 # This is how we declare value to a default variable 
+}
+
+output "op" {
+  value = var.a # This is how we access a variable
+}
